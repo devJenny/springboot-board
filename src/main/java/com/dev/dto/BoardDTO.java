70bb08f8de7eeca7cad9d8543ebc364034845876
devjenny <devje@naver.com>
2024-05-03 23:53:59 +0900

    Chore: content -> contents 수정

--- src/main/java/com/dev/dto/BoardDTO.java
+++ src/main/java/com/dev/dto/BoardDTO.java
@@ -15,7 +15,7 @@ public class BoardDTO {
     private String boardWriter;
     private String boardPass;
     private String boardTitle;
-    private String boardContent;
+    private String boardContents;
     private int boardHits;
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
