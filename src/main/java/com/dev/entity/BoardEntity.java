1a1be80f03ee66f8096a502e5adb80d1d2a568ee
devjenny <devje@naver.com>
2024-05-03 23:34:53 +0900

    Feat: entity 생성

---
+++ src/main/java/com/dev/entity/BoardEntity.java
@@ -0,0 +1,31 @@
+package com.dev.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+// DB의 테이블 역할을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity {
+    @Id // pk 컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(length=20, nullable = false) // 크기 20, not null
+    private String boardWriter;
+
+    @Column // 크기 255, null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    String boardContent;
+
+    @Column
+    private int boardHits;
+}
