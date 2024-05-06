954e267ef7347d520232721232f4d0e8c956ea6f
devjenny <devje@naver.com>
2024-05-06 14:58:13 +0900

    Feat: 페이징 요청 로직 추가

--- src/main/java/com/dev/controller/BoardController.java
+++ src/main/java/com/dev/controller/BoardController.java
@@ -77,6 +77,24 @@ public class BoardController {
     public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
 //        pageable.getPageNumber();
         Page<BoardDTO> boardList = boardService.paging(pageable);
-        return null;
+
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages(); // 3 6 9 ~~
+
+        // page 개수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 개수 3개
+        // 총 페이지 개수 8개
+        // 7 8
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "paging";
     }
 }
